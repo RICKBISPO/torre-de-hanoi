7f54969db6c66fe0585b37e7c2c4f1ec417b9fec
edenilsonjunior <edenilsonju10@gmail.com>
2023-11-25 07:44:55 -0300

    docs: documentacao das funcoes do jogo

--- jogo/include/jogo.h
+++ jogo/include/jogo.h
@@ -47,6 +47,9 @@
 */
 typedef SDL_Rect Button;
 
+/**
+ * Struct que representa todas as funcionalidades do jogo
+*/
 struct jogo{
     SDL_Window *window;
     SDL_Renderer *renderer;
@@ -62,36 +65,96 @@ struct jogo{
     int destino;
 };
 
+/**
+ * Ponteiro para a struct jogo
+*/
 typedef struct jogo* Game;
 
+/**
+ * Funcao que aloca espaco para uma struct jogo,
+ * inicializa a janela, renderizador, o icone, a fonte,
+ * as torres, os botoes e as variaveis de jogo
+*/
 Game iniciar_jogo();
 
+/**
+ * Inicializa 3 torres e enche a primeira torre com os discos = {3, 2, 1}
+*/
 void iniciar_torres(Pilha torres[]);
 
+/**
+ * Cria 6 botoes, 3 referentes as torres e 3 referentes aos botoes de
+ * reiniciar, desistir e sair.
+*/
 void iniciar_botoes(Button buttons[]);
 
+/**
+ * Limpa todas as alocações de memória que foram usadas
+*/
 void end_game(Game jogo);
 
+/**
+ * Limpa as alocações de memoria das torres e inicializa novas,
+ * além de resetar as variaveis de jogo
+*/
 void reset_game(Game jogo);
 
+/**
+ * Descobre se o mouse clicou em algum dos botoes
+*/
 int descobrir_click(Game jogo);
 
+/**
+ * Funcao que faz a logica de primeira ou segunda jogada, 
+ * considera se é a primeira jogada do jogador ou nao.
+*/
 void jogada(Game jogo, int temp);
 
+/**
+ * Funcao que verifica as regras do jogo, e se estiver certo, faz a jogada
+*/
 bool fazer_jogada(Pilha origem, Pilha destino);
 
+/**
+ * Funcao que resolve o jogo através de uma sequencia de passos,
+ * essa função é chamada apenas quando o usuario desiste do jogo
+*/
 void resolver_jogo(Game jogo);
 
+/**
+ * Funcao usada por resolver_jogo() que executa o gabarito da torre
+ * caso a primeira ou a segunda torres estejam ordenadas
+*/
 void gabarito_torre(Game jogo);
 
+/**
+ * Funcao que copia as informacoes da torre para um array, 
+ * usando pop e push
+*/
 void pilha_to_vetor(Pilha pilha, int array[]);
 
+/**
+ * Funcao usada por resolver_jogo() que faz algumas verificacoes 
+ * que diminuem o numero de jogadas necessarias
+*/
 void diminuir_jogada(Game jogo);
 
+/**
+ * Funcao que exibe as 3 torres com o SDL2
+*/
 void exibir_torres(Game jogo);
 
+/**
+ * Funcao que desenha os triangulos das torres
+*/
 void desenhar_retangulo(SDL_Renderer *renderer, int x, int y, int largura, int altura, int cores[]);
 
+/**
+ * Exibe o contador na tela
+*/
 void exibir_contador(Game jogo);
 
+/**
+ * Exibe uma sombra dos botoes na tela (Apenas para depuração)
+*/
 void exibir_botoes(Game jogo);
